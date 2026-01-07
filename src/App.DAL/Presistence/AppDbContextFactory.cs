69d727bb0b889ddf0af383c85ee07648ccc47d45
emilhuseyn <emiilhuseynov23@gmail.com>
2026-01-07 15:39:59 +0400

    ODdi

---
+++ src/App.DAL/Presistence/AppDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using App.DAL.Presistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace App.DAL.Presistence
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+    .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../App.API")) // API qovluğu
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseMySql(
+                connectionString,
+                new MySqlServerVersion(new Version(8, 0, 44))
+            );
+
+            // claimService null olaraq verilir, EF üçün kifayətdir
+            return new AppDbContext(builder.Options, claimService: null);
+        }
+    }
+}
